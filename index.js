8bc8112694b49b43b91a6b827b9a70ca2d2e5300
Forecasty <51519207+Forecasty@users.noreply.github.com>
2019-12-03 12:35:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// set constants from selenium-webdriver module
-// basically we have the selenium-webdriver module installed in node_modules directory
-// and this tells index.js (the current file) to allow us to use certain functions from
-// winthin that module i.e. Builder (to build a browser), By for locating elements
-// Key for pressing not text keys (i.e. RETURN) and util for general stuff I presume
-// them require("selenium-webdriver") is just telling us where to find those functions
+// the below line sets constants from the selenium-webdriver module so we can use it's functions
+// the selenium-webdriver module is installed in the node_modules directory after running npm install
+// this tells index.js (the current file) to allow us to use certain functions from within that module i.e.
+
+// Builder (to build a browser)
+// By for locating elements
+// Key for pressing non-text keys (i.e. RETURN)
+
+// require("selenium-webdriver") is just telling us where to find those functions (the node_modules directory)
+
 const {Builder, By, Key, util} = require("selenium-webdriver");
 
-// this is a function called example() which is called later when the script runs
+// this is a function called example() which is called when the script runs
 async function example(){
 
   // build the browser (firefox in our case)
@@ -15,8 +19,9 @@ async function example(){
   // navigate to Google after the browser is built
   await driver.get("https://google.com");
 
-  // findElement by name finds the search box on Google. If you look at inspect element
-  // you can see the input box has the name="q" tag. We could use by ID etc - anything that is unquily identifiable
+  // findElement by name finds the search box on Google.
+  // on inspect element you can see the input box has the name="q" attribute
+  // we could use by id="" etc - anything that is uniquely identifiable
   // then sendKeys enters the search query (brexit) and finally Key.Return hits enter
   await driver.findElement(By.name("q")).sendKeys("brexit", Key.RETURN);
 }
