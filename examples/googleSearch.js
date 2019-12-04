545a7d2e05a593b6eaf250357cbbd4156fcd5a53
Torlock <51519207+Forecasty@users.noreply.github.com>
2019-12-04 17:13:57 +0000

    Updated googleSearch.js

--- examples/googleSearch.js
+++ examples/googleSearch.js
@@ -1,30 +1,21 @@
-// the below line sets constants from the selenium-webdriver module so we can use it's functions
-// the selenium-webdriver module is installed in the node_modules directory after running npm install
-// this tells index.js (the current file) to allow us to use certain functions from within that module i.e.
-
-// Builder (to build a browser)
-// By for locating elements
-// Key for pressing non-text keys (i.e. RETURN)
-
-// require("selenium-webdriver") is just telling us where to find those functions (the node_modules directory)
-
+// import APIs from selenium-webdriver
 const {Builder, By, Key, util} = require("selenium-webdriver");
 
-// this is a function called example() which is called when the script runs
-async function example(){
+// create googleSearch function
+async function googleSearch(){
 
   // build the browser (firefox in our case)
   let driver = await new Builder().forBrowser("firefox").build();
 
-  // navigate to Google after the browser is built
+  // navigate to Google
   await driver.get("https://google.com");
 
-  // findElement by name finds the search box on Google.
-  // on inspect element you can see the input box has the name="q" attribute
-  // we could use by id="" etc - anything that is uniquely identifiable
-  // then sendKeys enters the search query (brexit) and finally Key.Return hits enter
-  await driver.findElement(By.name("q")).sendKeys("brexit", Key.RETURN);
+  // findElement(By.name("q")) finds the search box element on Google
+  // if you inspect the Google homepage you can see the search input box has the name="q" attribute
+  // we could use by ID attribute or XPath of the element etc. - anything that is uniquely identifiable
+  // sendKeys then enters the search query (askew) and finally Key.Return hits the return key
+  await driver.findElement(By.name("q")).sendKeys("askew", Key.RETURN);
 }
 
 // this calls the example() function above
-example();
+googleSearch();
